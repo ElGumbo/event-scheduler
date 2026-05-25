e5575e281400d3e3ccad74dc283ea44d9e0b2a28
Gumba <jeromontz@hotmail.de>
2026-05-25 13:38:14 +0200

    feat: sort events chronologically by date

--- src/components/events/EventList.jsx
+++ src/components/events/EventList.jsx
@@ -3,10 +3,12 @@ import EventCard from "./EventCard";
 
 export default function EventList({ events = [] }) {
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(24rem,1fr))] gap-6 p-20">
-      {events.map((event) => (
-        <EventCard key={event.id} event={event} />
-      ))}
+    <div className='grid grid-cols-[repeat(auto-fill,minmax(24rem,1fr))] gap-6 p-20'>
+      {[...events]
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
     </div>
   );
 }
